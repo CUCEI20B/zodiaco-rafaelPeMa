7812438ccb3afe71699faa73cbd71f1312df9c5c
Rafael <rafael.perez7499@alumnos.udg.mx>
2020-09-28 12:30:06 -0500

    prueba01

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
-print("Hola Mundo")
+dia = int(input())
+mes = int(input())
+
+if mes == 1:
+    if dia <= 19:
+        print("CAPRICORNIO")
+    else:
+        print("ACUARIO")
+elif mes == 2:
+    if dia <= 19:
+        print("ACUARIO")
+    else:
+        print("PISCIS")
+elif mes == 3:
+    if dia <= 20:
+        print("PISCIS")
+    else:
+        print("ARIES")
+elif mes == 4:
+    if dia <= 19:
+        print("ARIES")
+    else:
+        print("TAURO")
+elif mes == 5:
+    if dia <= 20:
+        print("TAURO")
+    else:
+        print("GEMINIS")
+elif mes == 6:
+    if dia <= 20:
+        print("GEMINIS")
+    else:
+        print("CANCER")
+elif mes == 7:
+    if dia <= 22:
+        print("CANCER")
+    else:
+        print("LEO")
+elif mes == 8:
+    if dia <= 22:
+        print("LEO")
+    else:
+        print("VIRGO")
+elif mes == 9:
+    if dia <= 22:
+        print("VIRGO")
+    else:
+        print("LIBRO")
+elif mes == 10:
+    if dia <= 22:
+        print("LIBRA")
+    else:
+        print("ESCORPIO")
+elif mes == 11:
+    if dia <= 21:
+        print("ESCORPIO")
+    else:
+        print("SAGITARIO")
+elif mes == 12:
+    if dia <= 21:
+        print("SAGITARIO")
+    else:
+        print("CAPRICORNIO")
